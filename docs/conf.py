f91fa17bcdef3c4eed6c0b9930f5d32a61740aab
Mateusz Sokół <8431159+mtsokol@users.noreply.github.com>
2024-12-03 18:05:32 +0100

    NLP: JAX transformer text classification (#103)

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ exclude_patterns = [
     'JAX_Vision_transformer.md',
     'JAX_machine_translation.md',
     'JAX_time_series_classification.md',
+    'JAX_transformer_text_classification.md',
 ]
 
 suppress_warnings = [
@@ -90,4 +91,5 @@ nb_execution_excludepatterns = [
     'JAX_Vision_transformer.ipynb',
     'JAX_machine_translation.ipynb',
     'JAX_time_series_classification.ipynb',
+    'JAX_transformer_text_classification.ipynb',
 ]
