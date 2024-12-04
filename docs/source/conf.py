20f38e912b8efc553c28cecc1c5b9c342da482f4
Wayne Wei <5577741+windmaple@users.noreply.github.com>
2024-12-04 12:56:17 +0800

    Fix typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,7 @@ exclude_patterns = [
     'digits_vae.md',
     'getting_started_with_jax_for_AI.md',
     'JAX_for_PyTorch_users.md',
+    'JAX_porting_HF_model.md',
     'JAX_porting_PyTorch_model.md',
     'digits_diffusion_model.md',
     'JAX_for_LLM_pretraining.md',
